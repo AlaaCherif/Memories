703bd31b4caa65a0ea7d8ca7c3114ab72ff5a8df
CHERIF <alaaeddinecherif@insat.u-carthage.tn>
2022-01-26 19:05:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './assets/memories.png';
+import Posts from './Components/Posts/Posts';
+import Form from './Components/Form/Form';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxwidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Memories
+        </Typography>
+        <img src={memories} alt='memories' height='60' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch'>
+            <Grid item xs={12} sm={4}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
-}
-
+};
 export default App;
